80c54e5e588f9509612230f1d6bd186613df6331
janosfk <dotnetjanos@hotmail.com>
2015-07-26 21:39:03 -0500

    Samples for ES6 map and generators

---
+++ src/server/generators.js
@@ -0,0 +1,12 @@
+function* fruitGenerator()
+{
+    yield 'apple';
+    yield 'orange';
+    return 'watermelon';
+
+}
+
+var newFruitGenerator = fruitGenerator();
+console.log(newFruitGenerator.next());
+console.log(newFruitGenerator.next());
+
