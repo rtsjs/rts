7b10d4a1ef35a3a180890e3a656afeff1c642f6d
janosfk <dotnetjanos@hotmail.com>
2015-08-09 10:58:31 -0500

    Hooked up karma, mocha, chai

---
+++ src/server/lib/priorityQueue.js
@@ -0,0 +1,23 @@
+module.exports = priorityQueue;
+
+function PriorityQueue(comparer) {
+    ;
+}
+
+
+
+PriorityQueue.prototype.queue = function () {
+
+
+}
+
+PriorityQueue.prototype.length = function () {
+
+npl
+}
+
+PriorityQueue.prototype.peek = function () {
+
+
+}
+
---
+++ test/server/lib/priorityQueue_spec.js
@@ -0,0 +1,14 @@
+var expect = chai.expect,
+    should = chai.should;
+
+describe('Priority queue', function () {
+
+    it('should pass dummy test', function () {
+        expect(1).to.equal(1);
+    })
+
+    it('should not pass dummy test', function () {
+        expect(1).to.equal(2);
+    })
+
+});
